835dce9dff7bc2157d6e175af82706df1064fd65
joejs92 <joejs_92@yahoo.com>
2024-05-02 17:40:15 -0400

    work with one error

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # Basic-Weather-App
 This is the Basic weather app from TOP
+
+Requirements: 
+
+1. Creating a weather forecast site using the weather API. What exactly should be included is left way open to interpretation. I think to start I will only include temperature, cloud condition, and the name of the city. 
+
+2. The location has to be searchable. This isn't too hard; just do what you did for the Giphy project. Got it done.
+
+3. Needs to toggle whether the temperature is in F or C. For this I will keep F and C values in separate lists and have the program switch between them when the button is toggled.
+
+4. The look of the page should change based on the data.
--- source.js
+++ source.js
@@ -1,37 +1,75 @@
+const tempF = [[]];
+const tempC = [[]];
 
-function getWeather(element){
-    const search = document.getElementById('search').value;
-    fetch(`https://api.weatherapi.com/v1/current.json?key=f7b033dfe0ff4f6e9b5135623242304&q=${search}`)
+function getWeather(){
+    const location = document.getElementById('search').value;
+    return new Promise(function(resolve){
+    fetch(`https://api.weatherapi.com/v1/current.json?key=f7b033dfe0ff4f6e9b5135623242304&q=${location}`)
         .then(function(response) {
             return response.json();
         })
         .then(function(response) {
-            const weatherValues = response.current;
-            const headers = Object.getOwnPropertyNames(weatherValues);
-            const values = Object.values(weatherValues);
-            
-            for(let i = 0; i<headers.length; i++){
-                const text = document.createElement('p');
-                text.textContent = `${headers[i]}: ${values[i]}`;
-                //console.log(`${headers[i]}: ${values[i]}`);
-                element.appendChild(text);
-            }
+            const currentTempF = [location, `${response.current.feelslike_f}° F`];
+            const currentTempC = [location, `${response.current.feelslike_c}° C`];
+            tempF.push(currentTempF);
+            tempC.push(currentTempC);
+            resolve(); 
         });
+    }) 
 }
 
 function makeWeatherBox() {
     const element = document.getElementById('bottom');
-    element.style.width = '540px';
-    element.style.padding = '4px';
-    element.style.margin='8px';
-    element.style.borderWidth = '1px';
-    element.style.borderStyle = 'solid';
-    element.style.borderColor = 'black';
-    return element;
+    const newElement = document.createElement('div');
+    newElement.setAttribute('id','weatherBox');
+    element.appendChild(newElement);
+    return newElement;
 }
 
-const button = document.querySelector('button');
-button.addEventListener('click',() => {
-    const element = makeWeatherBox()
-    getWeather(element);
+function deleteDomElement() {
+    const doomedElement = document.getElementById('weatherBox');
+    doomedElement.remove();
+}
+
+function deleteWeatherInfo() {
+    tempF.splice(0,1);
+    tempC.splice(0,1);
+}
+
+function addElements() {
+    const element = document.getElementById('weatherBox');
+    const city = document.createElement('h3');
+    city.textContent = tempF[0][0];
+    element.appendChild(city);
+
+    const temp = document.createElement('p');
+    temp.textContent = `Current temperature: ${tempF[0][1]} / ${tempC[0][1]}.`;
+    element.appendChild(temp);
+}
+
+makeWeatherBox();
+
+const div = document.querySelector('div');
+div.addEventListener('click', event => {
+    const target = event.target;
+    if(target.tagName == 'BUTTON') {
+        if(target.innerText == 'Search'){
+            // See https://www.youtube.com/watch?v=TnhCX0KkPqs
+            //for more info on how promises work.
+            deleteDomElement();
+            deleteWeatherInfo();
+            getWeather()
+                .then(makeWeatherBox)
+                .then(addElements)
+        };
+    };
 });
+
+/*
+Next steps. 
+1. Add a unique ID to the search button.
+2. Modify addElements to display only F as default.
+3. Modify the addElements function to add a button that
+converts to C when F is displayed, and to F when C is 
+displayed.
+*/
